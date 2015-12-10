6bbb10294e6f2560a40921949b4b44dd5051df60
DiegoAbal6 <diegoabalam@gmail.com>
2015-12-10 21:51:46 +0100

    DriverDetailFragment
    Recojo el TextView creado anteriormente

--- app/src/main/java/com/example/diego/examen/DriverDetailFragment.java
+++ app/src/main/java/com/example/diego/examen/DriverDetailFragment.java
@@ -59,9 +59,9 @@ public class DriverDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_driver_detail, container, false);
 
-        // Show the dummy content as text in a TextView.
+        
         if (mItem != null) {
-            ((TextView) rootView.findViewById(R.id.driver_detail)).setText(mItem.details);
+            ((TextView) rootView.findViewById(R.id.textView)).setText(mItem.id);
         }
 
         return rootView;
